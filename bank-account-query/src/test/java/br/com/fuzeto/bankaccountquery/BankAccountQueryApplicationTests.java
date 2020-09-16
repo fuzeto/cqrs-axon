db9e495fdcfb7ff1ce51cabacd7c905d743f5b51
Leonardo Fuzeto <fuzetolhf@gmail.com>
2020-09-16 10:09:28 -0300

    first commit with modules structure

---
+++ bank-account-query/src/test/java/br/com/fuzeto/bankaccountquery/BankAccountQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fuzeto.bankaccountquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountQueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
